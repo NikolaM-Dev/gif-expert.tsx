9c8418399f884375d8ef079a0e1da8d108db1895
Juan David Merchan Torres <juan1014166@gmail.com>
2023-04-04 16:43:17 -0500

    feat(GifExpertApp): Add AddCategory & GifGrid components

--- src/GifExpertApp.tsx
+++ src/GifExpertApp.tsx
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+
+import { AddCategory, GifGrid } from './components';
+
 interface IProps {}
 
 export const GifExpertApp = ({}: IProps): JSX.Element => {
+  const [categories, setCategories] = useState<string[]>(['One Punch']);
+
+  const handleAddCategory = (newCategory: string) => {
+    const category = newCategory.trim();
+
+    if (categories.includes(category)) return;
+    if (category.length < 2) return;
+
+    setCategories([category, ...categories]);
+  };
+
   return (
     <>
       <h1>GifExpertApp</h1>
+
+      <AddCategory handleAddCategory={handleAddCategory} />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
