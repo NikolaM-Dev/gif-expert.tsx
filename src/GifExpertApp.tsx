27618cc831572678c59cf41815641278e786e28f
Juan David Merchan Torres <juan1014166@gmail.com>
2023-04-04 16:06:32 -0500

    feat(app): Add GifExpertApp component

---
+++ src/GifExpertApp.tsx
@@ -0,0 +1,9 @@
+interface IProps {}
+
+export const GifExpertApp = ({}: IProps): JSX.Element => {
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,6 @@ import './styles.css';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <h1>Gif Expert</h1>
+    <GifExpertApp />
   </React.StrictMode>,
 );
