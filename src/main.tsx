4f71eda1f7f153286bbb7eca3dcf07dd55f7a1f4
Juan David Merchan Torres <juan1014166@gmail.com>
2023-04-04 16:06:05 -0500

    feat(app): Add global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { GifExpertApp } from './GifExpertApp';
+
+import './styles.css';
+
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <h1>Gif Expert</h1>
