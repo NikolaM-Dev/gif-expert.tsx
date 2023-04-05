70d793e25278f4a82cf298ed27e9a219e4a85dd8
Juan David Merchan Torres <juan1014166@gmail.com>
2023-04-05 14:58:44 -0500

    refactor(services): Change to use constants config

--- src/services/get-gifs.service.ts
+++ src/services/get-gifs.service.ts
@@ -1,14 +1,12 @@
+import { c } from '../config/constants';
 import { Gif, IGif } from '../models';
 
-const GHIPHY_URL = 'https://api.giphy.com/v1/gifs/search';
-const API_KEY = 'pjZygRY64aJ1Fsdp1JPJkKFTOA3wv8rA';
-
 export const getGifs = async (category: string): Promise<IGif[]> => {
-  const url = `${GHIPHY_URL}?api_key=${API_KEY}&q=${category}&=limit=10`;
+  const url = `${c.giphyUrl}?api_key=${c.apiKey}&q=${category}&=limit=10`;
   const resp = await fetch(url);
-  const { data } = await resp.json();
+  const { data: rawGifs } = await resp.json();
 
-  return data.map(({ id, title, images }: Gif) => ({
+  return rawGifs.map(({ id, title, images }: Gif) => ({
     id,
     title,
     url: images.downsized_medium.url,
