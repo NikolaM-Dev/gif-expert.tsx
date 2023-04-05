6788779a16f5d45641559b1b9e37c516cb71d529
Juan David Merchan Torres <juan1014166@gmail.com>
2023-04-05 14:59:35 -0500

    feat(GifGrid): Add base gifs render

--- src/components/GifGrid.tsx
+++ src/components/GifGrid.tsx
@@ -1,12 +1,28 @@
+import { useState, useEffect } from 'react';
+
+import { getGifs } from '../services';
+import { IGif } from '../models';
+
 interface IProps {
   category: string;
 }
 
 export const GifGrid = ({ category }: IProps): JSX.Element => {
+  const [gifs, setGifs] = useState<IGif[]>([]);
+
+  useEffect(() => {
+    getGifs(category).then(setGifs);
+  }, [setGifs]);
+
   return (
     <>
       <h2>{category}</h2>
-      <p>Hola Mundo</p>
+      {gifs.map(({ title, id, url }: IGif) => (
+        <article key={id}>
+          <h3>{title}</h3>
+          <img src={url} alt={title} />
+        </article>
+      ))}
     </>
   );
 };
