56e3ae78d566e6fadd91512a3d79af14667bc5bf
Juan David Merchan Torres <juan1014166@gmail.com>
2023-04-11 07:45:41 -0500

    test(AddCategory): Add call handleAddCategory if input have any value

--- __tests__/components/AddCategory.spec.tsx
+++ __tests__/components/AddCategory.spec.tsx
@@ -20,4 +20,19 @@ describe('Tests on <AddCategory />', () => {
 
     expect(input.value).toBe(category);
   });
+
+  it('should to call handleAddCategory if input have any value', () => {
+    const handleAddCategory = jest.fn();
+    render(<AddCategory handleAddCategory={handleAddCategory} />);
+
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+    const form = screen.getByRole('form') as HTMLFormElement;
+
+    fireEvent.input(input, { target: { value: category } });
+    fireEvent.submit(form);
+
+    expect(input.value).toBe('');
+    expect(handleAddCategory).toHaveBeenCalledTimes(1);
+    expect(handleAddCategory).toHaveBeenCalledWith(category);
+  });
 });
