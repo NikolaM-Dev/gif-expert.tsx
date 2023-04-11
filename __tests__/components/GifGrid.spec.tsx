27522c5c8e0fcfcc105d5d99002137e08d12048a
Juan David Merchan Torres <juan1014166@gmail.com>
2023-04-11 09:12:17 -0500

    test(GifGrid): Add useFetchGifs mock

--- __tests__/components/GifGrid.spec.tsx
+++ __tests__/components/GifGrid.spec.tsx
@@ -1,17 +1,31 @@
 import { render, screen } from '@testing-library/react';
 
 import { GifGrid } from '../../src/components';
+import { IGif } from '../../src/models';
+import { useFetchGifs } from '../../src/hooks';
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Tests on <GifGrid />', () => {
   const category = 'Dragon Ball Z';
 
   it('should to match with snapshot', () => {
+    (useFetchGifs as jest.Mock).mockReturnValue({
+      gifs: [],
+      isLoading: true,
+    });
+
     const { container } = render(<GifGrid category={category} />);
 
     expect(container).toMatchSnapshot();
   });
 
   it('should to show initial loading', () => {
+    (useFetchGifs as jest.Mock).mockReturnValue({
+      gifs: [],
+      isLoading: true,
+    });
+
     render(<GifGrid category={category} />);
 
     expect(screen.getByText('Is Loading...')).toBeTruthy();
