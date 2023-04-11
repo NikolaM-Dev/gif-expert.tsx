1b6fd58ad037bf0d2d1e528f54c3a4e825f80cc3
Juan David Merchan Torres <juan1014166@gmail.com>
2023-04-11 09:12:27 -0500

    test(GifGrid): Add show gifs test

--- __tests__/components/GifGrid.spec.tsx
+++ __tests__/components/GifGrid.spec.tsx
@@ -8,6 +8,23 @@ jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Tests on <GifGrid />', () => {
   const category = 'Dragon Ball Z';
+  const gifs: IGif[] = [
+    {
+      id: 'gwx8M5dU53PtS',
+      title: 'dragon ball z GIF',
+      url: 'https://media2.giphy.com/media/gwx8M5dU53PtS/giphy.gif?cid=6eaa0c54fkrf4938t5p98tzd5560l9qo0b3jb5jrxcpwt5wp&rid=giphy.gif&ct=g',
+    },
+    {
+      id: 'ixYRj3H9HOzWE',
+      title: 'dragon ball z GIF',
+      url: 'https://media1.giphy.com/media/ixYRj3H9HOzWE/giphy.gif?cid=6eaa0c54fkrf4938t5p98tzd5560l9qo0b3jb5jrxcpwt5wp&rid=giphy.gif&ct=g',
+    },
+    {
+      id: 'pz1s2IpdQh86k',
+      title: 'dragon ball z GIF',
+      url: 'https://media1.giphy.com/media/pz1s2IpdQh86k/giphy.gif?cid=6eaa0c54fkrf4938t5p98tzd5560l9qo0b3jb5jrxcpwt5wp&rid=giphy.gif&ct=g',
+    },
+  ];
 
   it('should to match with snapshot', () => {
     (useFetchGifs as jest.Mock).mockReturnValue({
@@ -30,5 +47,16 @@ describe('Tests on <GifGrid />', () => {
 
     expect(screen.getByText('Is Loading...')).toBeTruthy();
     expect(screen.getByText(category)).toBeTruthy();
+  });
+
+  it('should to show gif items when load gifs from useFetchGifs', () => {
+    (useFetchGifs as jest.Mock).mockReturnValue({
+      gifs,
+      isLoading: false,
+    });
+
+    render(<GifGrid category={category} />);
+
+    expect(screen.getAllByRole('img')).toHaveLength(3);
   });
 });
